e1551b635f104d6dc04cf4c828ebc5cb3c3d69a5
Anderson Pablo <mib_anderson@hotmail.com>
2016-07-27 14:28:48 -0300

    JUnit - Parameterized Test

---
+++ testeJunit/src/com/anderson/junit/parametized/PrimeNumberChecker.java
@@ -0,0 +1,12 @@
+package com.anderson.junit.parametized;
+
+public class PrimeNumberChecker {
+	public Boolean validate(final Integer primeNumber) {
+		for (int i = 2; i < (primeNumber / 2); i++) {
+			if (primeNumber % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
